7eebfedf43f0383d60e5e8a1b789ff4811eff6ce
Hiroki Kondo <kondo.matr02@gmail.com>
2019-11-19 19:02:45 +0900

    Initial Commit

---
+++ GitTestApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GitTestApp
+//
+//  Created by 宏輝 on 19/11/2019.
+//  Copyright © 2019 宏輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+/*藤井さん
+    
+    NextViewControllerに
+     var acceptString　＝ String()
+     と書いてください。
+     
+     それだけです。
+  */
+}
+
